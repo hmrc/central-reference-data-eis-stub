8cca3b62420aed24c6494e820f39d4ed2bae535f
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2024-09-09 17:30:47 +0100

    CRDL-139: Bearer Token is now required.

--- it/test/uk/gov/hmrc/centralreferencedataeisstub/controllers/InboundControllerISpec.scala
+++ it/test/uk/gov/hmrc/centralreferencedataeisstub/controllers/InboundControllerISpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.centralreferencedataeisstub.controllers
 
 import org.scalatest.wordspec.AnyWordSpec
@@ -9,6 +25,7 @@ import play.api.http.HeaderNames
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
 import play.api.libs.ws.DefaultBodyWritables.*
+import uk.gov.hmrc.centralreferencedataeisstub.config.AppConfig
 
 class InboundControllerISpec
   extends AnyWordSpec,
@@ -18,6 +35,7 @@ class InboundControllerISpec
     GuiceOneServerPerSuite:
 
   private val wsClient = app.injector.instanceOf[WSClient]
+  private val appConfig = app.injector.instanceOf[AppConfig]
   private val baseUrl = s"http://localhost:$port"
 
   private val validTestBody: scala.xml.Elem = <MainMessage>
@@ -41,7 +59,8 @@ class InboundControllerISpec
           .url(s"$baseUrl/central-reference-data-eis-stub/services/crdl/referencedataupdate/v1")
           .withHttpHeaders(
             HeaderNames.ACCEPT -> "application/xml",
-            HeaderNames.CONTENT_TYPE -> "application/xml"
+            HeaderNames.CONTENT_TYPE -> "application/xml", 
+            HeaderNames.AUTHORIZATION -> appConfig.bearerToken
           )
           .post(validTestBody.toString)
           .futureValue
